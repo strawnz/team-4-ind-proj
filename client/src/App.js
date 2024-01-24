1cd9f2487dc35917cdcc34bd42aabe0be702a4db
C.J. Astronomo <cj.astronomo@gmail.com>
2024-01-24 17:02:05 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Control Alt Defeat's Industry Project Placeholder</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+// GET test to Postman
+app.get('/', (req, res) => {
+    res.send('This is a GET request to root');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+});
