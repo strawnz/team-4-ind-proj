dfbfd938c0e40855d8b05802cc0947b4abbe0df7
Jamie Ingalls <jamesmaclay@gmail.com>
2024-01-26 13:29:01 -0500

    cards-top done, bottoms started

--- client/src/pages/DestinationsPage/DestinationsPage.js
+++ client/src/pages/DestinationsPage/DestinationsPage.js
@@ -1,9 +1,11 @@
 import './DestinationsPage.scss';
+import DestinationsdCards from "../../components/Destinations/Destinations";
 
 function DestinationsPage() {
     return (
         <>
-        <h1>Destinations Page Placeholder</h1>
+            <h1>Destinations Page Placeholder</h1>
+            <DestinationsdCards />
         </>
     )
 }
