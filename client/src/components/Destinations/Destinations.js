bc87a27cc67c3b9365a93ddec0debb0f9a9d01aa
Jamie Ingalls <jamesmaclay@gmail.com>
2024-01-26 11:53:32 -0500

    Destination Components

---
+++ client/src/components/Destinations/Destinations.js
@@ -0,0 +1,10 @@
+function Destinations() {
+  return (
+    <section className="destinations">
+      <div className="destinations-card" ></div>
+      <div className="destinations-card" ></div>
+      <div className="destinations-card" ></div>
+      <div className="destinations-card" ></div>
+    </section>
+  )
+}
